15b77662ae1744d62e6ae915ccdcadcc61bfee3b
Vitor Livi <vitorlivi1999@gmail.com>
2022-10-12 10:38:55 -0300

    feat: create test to category in-memory repository

--- src/category/infra/repository/category-in-memory.repository.ts
+++ src/category/infra/repository/category-in-memory.repository.ts
@@ -1,3 +1,4 @@
+import { SortDirection } from "shared/domain/repository/repository-contracts";
 import { InMemorySearchableRepository } from "../../../shared/domain/repository/in-memory.repository";
 import { Category } from "../../domain/entities/category";
 import CategoryRepository from "../../domain/repository/category.repository";
@@ -5,6 +6,8 @@ import CategoryRepository from "../../domain/repository/category.repository";
 export default class CategoryInMemoryRepository
 extends InMemorySearchableRepository<Category>
 implements CategoryRepository.Repository {
+  sortableFields: string[] = ["name", "created_at"];
+
   protected async applyFilter(
     items: Category[],
     filter: CategoryRepository.Filter
@@ -17,4 +20,16 @@ implements CategoryRepository.Repository {
       return i.name.toLowerCase().includes(filter.toLowerCase());
     });
   }
-}
+
+  protected async applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Promise<Category[]> {
+    if (!sort) {
+      return super.applySort(items, "created_at", "desc");
+    }
+
+    return super.applySort(items, sort, sort_dir);
+  }
+} 
