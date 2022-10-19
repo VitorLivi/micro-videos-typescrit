b79bf224ed58c0b2140d306b88a1867c42c83cc7
Vitor Livi <vitorlivi1999@gmail.com>
2022-10-18 21:09:54 -0300

    feat: create test for create category use case

--- src/category/application/create-category.usecase.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from '../domain/entities/category';
-
-class CreateCategoryUseCase {
-  execute(input) {
-    const entity = new Category(input);
-  }
-}
---
+++ src/category/application/use-cases/__tests__/create-category.use-case.spec.ts
@@ -0,0 +1,36 @@
+import CategoryInMemoryRepository from '../../../infra/repository/category-in-memory.repository';
+import CreateCategoryUseCase from '../create-category.use-case';
+
+describe('CreateCategoryUseCase', () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  it('should create a category', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert');
+
+    let output = await useCase.execute({name: 'test'});
+    expect(output).toStrictEqual({
+      id: repository.items.at(0).id,
+      name: 'test',
+      description: null,
+      is_active: true,
+      created_at: repository.items.at(0).created_at
+    });
+    expect(spyInsert).toHaveBeenCalledTimes(1);
+
+    output = await useCase.execute({name: 'test', description: 'test', is_active: false});
+    expect(output).toStrictEqual({
+      id: repository.items.at(1).id,
+      name: 'test',
+      description: 'test',
+      is_active: false,
+      created_at: repository.items.at(1).created_at
+    });
+    expect(spyInsert).toHaveBeenCalledTimes(2);
+  });
+});
