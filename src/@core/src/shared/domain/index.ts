4bff74510b927b6653c97bbd50dafe935b1a5c1f
Vitor Livi <vitorlivi1999@gmail.com>
2022-11-18 23:01:05 -0300

    feat: configure core export subpaths

--- src/@core/src/shared/domain/index.ts
+++
@@ -1,7 +0,0 @@
-// created from 'create-ts-index'
-
-export * from './errors';
-export * from './repository';
-export * from './utils';
-export * from './validators';
-export * from './value-objects';
