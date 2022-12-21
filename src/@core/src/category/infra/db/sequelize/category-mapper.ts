8a4b35bc7b1a7a3277982f0cdf67aa485319365f
Vitor Livi <vitor@pop-os.localdomain>
2022-12-20 23:13:03 -0300

    feat: change validation error type when in repository

--- src/@core/src/category/infra/db/sequelize/category-mapper.ts
+++ src/@core/src/category/infra/db/sequelize/category-mapper.ts
@@ -1,11 +1,20 @@
 import { Category } from "#category/domain";
-import {UniqueEntityId} from "#shared/domain";
+import { EntityValidationError, UniqueEntityId } from "#shared/domain";
+import {LoadEntityError} from "#shared/domain/errors/load-entity.error";
 import { CategoryModel } from "./category-model";
 
 export class CategoryModelMapper {
   static toEntity(model: CategoryModel){
     const {id, ...otherData} = model.toJSON()
+  
+    try {
+      return new Category(otherData, new UniqueEntityId(id))
+    } catch (error) {
+      if (error instanceof EntityValidationError) {
+        throw new LoadEntityError(error.error, error.message);
+      }
 
-    return new Category(otherData, new UniqueEntityId(id))
+      throw error;
+    }
   }
 }
